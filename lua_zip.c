26671ffaa62a3297a47c4e52647e2261316ea400
zyxwvu Shi <i@shiyc.cn>
2020-06-18 17:12:27 +0800

    Remove brimworks prefix.

--- lua_zip.c
+++ lua_zip.c
@@ -788,7 +788,7 @@ static int S_OR(lua_State* L) {
     return 1;
 }
 
-LUALIB_API int luaopen_brimworks_zip(lua_State* L) {
+LUALIB_API int luaopen_zip(lua_State* L) {
     static luaL_Reg fns[] = {
         { "open",     S_archive_open },
         { "OR",       S_OR },
